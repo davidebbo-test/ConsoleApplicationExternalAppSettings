baaa7626d12c5d200766da8540acbdcfe54aeaba
davidebbo <david.ebbo@microsoft.com>
2015-08-11 15:55:50 -0700

    Initial commit

---
+++ ConsoleApplicationExternalAppSettings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationExternalAppSettings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Foo = {0}", ConfigurationManager.AppSettings["foo"]);
+            Console.WriteLine("Bar = {0}", ConfigurationManager.AppSettings["bar"]);
+        }
+    }
+}
